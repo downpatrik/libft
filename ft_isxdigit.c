17668f83391b117f5290874da68e2735058100a4
Wayn Venita <wvenita@at-o9.21-school.ru>
2019-04-15 22:50:38 +0300

    add atof, isxdigit

--- ft_isxdigit.c
+++ ft_isxdigit.c
@@ -6,13 +6,13 @@
 /*   By: wvenita <wvenita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 22:11:00 by wvenita           #+#    #+#             */
-/*   Updated: 2019/04/15 22:19:10 by wvenita          ###   ########.fr       */
+/*   Updated: 2019/04/15 22:50:18 by wvenita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		isxdigit(int c)
+int		ft_isxdigit(int c)
 {
 	return (ft_isdigit(c) || (c > 64 && c < 71) || (c > 96 && c < 103));
 }
